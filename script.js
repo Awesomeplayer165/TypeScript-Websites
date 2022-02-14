a715d783b848c93ddddd0c2aa1a898cef6c0c9b4
Awesomeplayer165 <jacobtrentinidevelopment@gmail.com>
2022-02-13 20:44:44 -0800

    Added helloWorld function

---
+++ script.js
@@ -0,0 +1,4 @@
+function helloWorld(a, b) {
+    console.log(a, b);
+}
+helloWorld(3, "Hello");
---
+++ script.ts
@@ -0,0 +1,5 @@
+function helloWorld(a: number, b: string) {
+    console.log(a, b)
+}
+
+helloWorld(3, "Hello")
